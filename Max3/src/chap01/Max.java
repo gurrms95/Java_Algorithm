e9970cc8b2409a4c4e04288ab0b581e7e00c5582
gurrms95 <gurrms95@gmail.com>
2020-07-29 23:51:33 +0900

    java

---
+++ Max3/src/chap01/Max.java
@@ -0,0 +1,23 @@
+package chap01;
+
+import java.util.Scanner;
+
+class Max {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("세 정수의 최댓값을 구하시오");
+		System.out.print("a의 값 : "); int a= stdIn.nextInt();
+		System.out.print("b의 값 : "); int b= stdIn.nextInt();
+		System.out.print("c의 값 : "); int c= stdIn.nextInt();
+		
+		int max =a;
+		if(b>max) max=b;
+		if(c>max) max=c;
+		
+		System.out.println("최댓값은"+ max +"입니다.");
+	}
+
+}
