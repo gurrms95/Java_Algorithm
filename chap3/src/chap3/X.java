f711cc6a348609ec45478960107d14ae2193b2ee
gurrms95 <gurrms95@gmail.com>
2020-07-31 22:01:25 +0900

    update_C3

---
+++ chap3/src/chap3/X.java
@@ -0,0 +1,12 @@
+package chap3;
+
+public class X {
+	public static final Comparator<T> COMPARATOR = new Comp();
+	
+	private static class Comp implements Comparator<T>{
+		public int compare(T d1, T d2) {
+			
+		}
+	}
+
+}
